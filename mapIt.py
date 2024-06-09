dd5061eb6f1a201c1cfcf454259493f971fb54cf
Aryan Singh <aryan7109singh@gmail.com>
2024-06-09 10:45:55 +0530

    mapIt.py complete

--- mapIt.py
+++ mapIt.py
@@ -1,2 +1,14 @@
-import webbrowser
-webbrowser.open('https://inventwithpython.com/')
+import webbrowser, sys, pyperclip
+
+# webbrowser.open('https://inventwithpython.com/')
+
+print(sys.argv)
+
+if len(sys.argv) > 1:
+	address = ' '.join(sys.argv[1:])
+else:
+	address = pyperclip.paste()
+
+print(address)
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
